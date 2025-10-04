f44eb87cc6c9bac43dd48ded8436e33d5da6cf39
mshnwq <mshnwq.com@gmail.com>
2025-10-04 18:46:02 +0300

    fix conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,52 +1,52 @@
-local util = require("conform.util")
 -- https://github.com/stevearc/conform.nvim#setupopts
-local options = {
-  default_format_opts = {
-    timeout_ms = 3000,
-    lsp_format = "fallback",
-    quiet = false,
-    async = false,
-  },
-  -- format_on_save = {
-  -- 	lsp_fallback = true,
-  -- 	async = false,
-  -- 	timeout_ms = 500,
-  -- },
-  formatters_by_ft = {
-    lua = { "stylua" },
-    nix = { "nixfmt" }, -- nixfmt official (haskel), alejandra unofficial (rust)
-    bash = { "shfmt" },
-    sh = { "shfmt" },
-    python = { "ruff_format" },
-    -- use the lsp
-    -- go = { "gofumpt", "goimports-reviser", "golines" },
-    -- gomod = { "gofumpt", "goimports-reviser" },
-    -- gowork = { "gofumpt", "goimports-reviser" },
-    -- gotmpl = { "gofumpt", "goimports-reviser" },
-    yaml = { "prettierd" },
-    yml = { "prettierd" },
-    dockerfile = { "dockerfile-utils" },
-    -- javascript = { "prettierd" },
-    -- typescript = { "prettierd" },
-    -- css = { "prettierd" },
-    -- html = { "prettierd" },
-    -- json = { "prettierd" },
-  },
-  formatters = {
-    -- https://github.com/mvdan/sh
-    shfmt = {
-      inherit = false,
-      command = "shfmt",
-      args = { "-i", "2", "-filename", "$FILENAME" },
-    },
-    ["dockerfile-utils"] = {},
-    golines = {
-      prepend_args = { "--max-len=80" },
-    },
-    ["goimports-reviser"] = {
-      prepend_args = { "-rm-unused" },
-    },
-  },
+return {
+	"stevearc/conform.nvim",
+	event = "BufWritePre",
+	opts = {
+		log_level = vim.log.levels.DEBUG,
+		format_on_save = {
+		  lsp_fallback = true,
+		  async = false,
+		},
+		default_format_opts = {
+			timeout_ms = 3000,
+			lsp_format = "fallback",
+			quiet = false,
+			async = false,
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			nix = { "nixfmt" }, -- nixfmt official (haskel), alejandra unofficial (rust)
+			bash = { "shfmt" },
+			sh = { "shfmt" },
+			python = { "isort", "ruff_format" },
+			-- go = { "gofumpt", "goimports-reviser", "golines" },
+			-- gomod = { "gofumpt", "goimports-reviser" },
+			-- gowork = { "gofumpt", "goimports-reviser" },
+			-- gotmpl = { "gofumpt", "goimports-reviser" },
+			-- web --
+			-- javascript = { "prettierd" },
+			-- typescript = { "prettierd" },
+			-- css = { "prettierd" },
+			-- html = { "prettierd" },
+			-- json = { "prettierd" },
+			-- devops --
+			yaml = { "prettierd" },
+			yml = { "prettierd" },
+			["yaml.docker-compose"] = { "prettierd" },
+			["yaml.gitlab"] = { "prettierd" },
+			dockerfile = { "dockerfmt" },
+		},
+		formatters = {
+			nixfmt = {
+				append_args = { "--width", "80" },
+			},
+			-- golines = {
+			--   prepend_args = { "--max-len=80" },
+			-- },
+			-- ["goimports-reviser"] = {
+			--   prepend_args = { "-rm-unused" },
+			-- },
+		},
+	},
 }
-
-return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,210 +1,205 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("configs.treesitter")
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("configs.lspconfig")
-    end,
-  },
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("configs.lint")
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require("configs.conform")
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("configs.treesitter")
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("configs.lint")
+		end,
+	},
+	{
+		require("configs.conform"),
+	},
 
-  -- My Plugins --
-  {
-    "folke/todo-comments.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    -- config = function()
-    --   require("todo-comments").setup()
-    -- end,
-    opts = {
-      signs = true,      -- show icons in the signs column
-      sign_priority = 8, -- sign priority
-      -- keywords recognized as todo comments
-      keywords = {
-        FIX = {
-          icon = " ", -- icon used for the sign, and in search results
-          color = "error", -- can be a hex color, or a named color (see below)
-          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords
-          -- signs = false, -- configure signs for some keywords individually
-        },
-        TODO = { icon = " ", color = "info" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-        HACK = { icon = " ", color = "hint" },
-        PERF = { icon = " ", color = "hint", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-        TEST = { icon = "󰂓 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-      },
-      colors = {
-        error = { "TodoError" },
-        warning = { "TodoWarn" },
-        info = { "TodoInfo" },
-        hint = { "TodoHint" },
-        test = { "TodoTest" },
-        default = { "TodoDefault" },
-      },
-    },
-  },
-  -- {
-  --   "OXY2DEV/markview.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("markview").setup({
-  --       preview = {
-  --         enable = false,
-  --       },
-  --       markdown = {
-  --         headings = require("markview.presets").headings.glow
-  --       }
-  --     });
-  --     require("markview.extras.checkboxes").setup();
-  --   end,
-  -- },
-  {
-    'towolf/vim-helm',
-    ft = 'helm'
-  },
-  {
-    "folke/ts-comments.nvim",
-    event = "VeryLazy",
-    enabled = vim.fn.has("nvim-0.10.0") == 1,
-    opts = {
-      lang = {
-        terraform = { "# %s" },
-        yuck = { "; %s" },
-        ron = { "// %s" },
-      },
-    },
-  },
-  {
-    "rachartier/tiny-code-action.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope.nvim" },
-    },
-    event = "LspAttach",
-    config = function()
-      require('tiny-code-action').setup()
-    end
-  },
+	-- My Plugins --
+	{
+		"folke/todo-comments.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		-- config = function()
+		--   require("todo-comments").setup()
+		-- end,
+		opts = {
+			signs = true, -- show icons in the signs column
+			sign_priority = 8, -- sign priority
+			-- keywords recognized as todo comments
+			keywords = {
+				FIX = {
+					icon = " ", -- icon used for the sign, and in search results
+					color = "error", -- can be a hex color, or a named color (see below)
+					alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords
+					-- signs = false, -- configure signs for some keywords individually
+				},
+				TODO = { icon = " ", color = "info" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+				HACK = { icon = " ", color = "hint" },
+				PERF = { icon = " ", color = "hint", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+				TEST = { icon = "󰂓 ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+			},
+			colors = {
+				error = { "TodoError" },
+				warning = { "TodoWarn" },
+				info = { "TodoInfo" },
+				hint = { "TodoHint" },
+				test = { "TodoTest" },
+				default = { "TodoDefault" },
+			},
+		},
+	},
+	-- {
+	--   "OXY2DEV/markview.nvim",
+	--   lazy = false,
+	--   config = function()
+	--     require("markview").setup({
+	--       preview = {
+	--         enable = false,
+	--       },
+	--       markdown = {
+	--         headings = require("markview.presets").headings.glow
+	--       }
+	--     });
+	--     require("markview.extras.checkboxes").setup();
+	--   end,
+	-- },
+	{
+		"towolf/vim-helm",
+		ft = "helm",
+	},
+	{
+		"folke/ts-comments.nvim",
+		event = "VeryLazy",
+		enabled = vim.fn.has("nvim-0.10.0") == 1,
+		opts = {
+			lang = {
+				terraform = { "# %s" },
+				yuck = { "; %s" },
+				ron = { "// %s" },
+			},
+		},
+	},
+	{
+		"rachartier/tiny-code-action.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+		event = "LspAttach",
+		config = function()
+			require("tiny-code-action").setup()
+		end,
+	},
 
-  -- https://github.com/b0o/nvim-tree-preview.lua
+	-- https://github.com/b0o/nvim-tree-preview.lua
 
-  {
-    require("configs.nvimtree")
-  },
+	{
+		require("configs.nvimtree"),
+	},
 
-  {
-    require("configs.neominimap")
-  },
+	{
+		require("configs.neominimap"),
+	},
 
-  {
-    "rcarriga/nvim-notify",
-    config = function()
-      require("notify").setup({
-        background_colour = "#000000",
-      })
-    end
-  },
-  {
-    "folke/noice.nvim",
-    enabled = true,
-    lazy = false, -- NOTE: NO NEED to Lazy load
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      return require("configs.noice")
-    end,
-  },
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("notify").setup({
+				background_colour = "#000000",
+			})
+		end,
+	},
+	{
+		"folke/noice.nvim",
+		enabled = true,
+		lazy = false, -- NOTE: NO NEED to Lazy load
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+		config = function()
+			return require("configs.noice")
+		end,
+	},
 
-  {
-    "NeogitOrg/neogit",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-    config = function()
-      require('neogit').setup()
-    end,
-  },
-  {
-    "Juksuu/worktrees.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("worktrees").setup()
-    end,
-  },
+	{
+		"NeogitOrg/neogit",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = function()
+			require("neogit").setup()
+		end,
+	},
+	{
+		"Juksuu/worktrees.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("worktrees").setup()
+		end,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    opts = function()
-      return require("configs.cmp")
-    end,
-  },
-  --{
-  --  "zbirenbaum/copilot.lua",
-  --  cmd = "Copilot",
-  --  event = "InsertEnter",
-  --  config = function()
-  --    return require("configs.copilot")
-  --  end,
-  --},
-  --{
-  --  "zbirenbaum/copilot-cmp",
-  --  event = "InsertEnter",
-  --  config = function()
-  --    require("copilot_cmp").setup()
-  --  end,
-  --  dependencies = {
-  --    "zbirenbaum/copilot.lua",
-  --    -- cmd = "Copilot",
-  --    -- config = function()
-  --    --   require("copilot").setup({
-  --    --     suggestion = { enabled = false },
-  --    --     panel = { enabled = false },
-  --    --   })
-  --    -- end,
-  --  },
-  --},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		opts = function()
+			return require("configs.cmp")
+		end,
+	},
+	--{
+	--  "zbirenbaum/copilot.lua",
+	--  cmd = "Copilot",
+	--  event = "InsertEnter",
+	--  config = function()
+	--    return require("configs.copilot")
+	--  end,
+	--},
+	--{
+	--  "zbirenbaum/copilot-cmp",
+	--  event = "InsertEnter",
+	--  config = function()
+	--    require("copilot_cmp").setup()
+	--  end,
+	--  dependencies = {
+	--    "zbirenbaum/copilot.lua",
+	--    -- cmd = "Copilot",
+	--    -- config = function()
+	--    --   require("copilot").setup({
+	--    --     suggestion = { enabled = false },
+	--    --     panel = { enabled = false },
+	--    --   })
+	--    -- end,
+	--  },
+	--},
 
-  -- {
-  --   "jackMort/ChatGPT.nvim",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("chatgpt").setup()
-  --     require("chatgpt").setup({
-  --       api_key_cmd = 'pass show mshnwq/chatgpt-api',
-  --     })
-  --   end,
-  --   dependencies = {
-  --     "MunifTanjim/nui.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "folke/trouble.nvim", -- optional
-  --     "nvim-telescope/telescope.nvim"
-  --   }
-  -- },
-
+	-- {
+	--   "jackMort/ChatGPT.nvim",
+	--   event = "VeryLazy",
+	--   config = function()
+	--     require("chatgpt").setup()
+	--     require("chatgpt").setup({
+	--       api_key_cmd = 'pass show mshnwq/chatgpt-api',
+	--     })
+	--   end,
+	--   dependencies = {
+	--     "MunifTanjim/nui.nvim",
+	--     "nvim-lua/plenary.nvim",
+	--     "folke/trouble.nvim", -- optional
+	--     "nvim-telescope/telescope.nvim"
+	--   }
+	-- },
 }
