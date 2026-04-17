590929912d39e6663a95e2c0541f88228327383f
mshnwq <mshnwq.com@gmail.com>
2026-04-18 02:00:15 +0300

    oo

--- lua/configs/obsidian.lua
+++ lua/configs/obsidian.lua
@@ -1,5 +1,6 @@
 -- lua/configs/obsidian.lua
 local vault = "~/Documents/Obsidian/Home"
+local inbox = "0_Inbox"
 local M = {
 	keys = {
 		{ "<leader>on", ":Obsidian new " }, -- args: title
@@ -39,11 +40,50 @@ local M = {
 		},
 		open = {
 			use_advanced_uri = true,
-			func = vim.ui.open,
-			schemes = { "https", "http", "file", "mailto" },
+			-- BUG: when toggling on same line,
+			-- it returns to last previous line uri command
+			func = function(uri)
+				vim.ui.open(uri)
+				if uri:find("advanced%-uri") and uri:find("line=") then
+					local line = tonumber(uri:match("line=(%d+)"))
+					if line then
+						local encoded_vault = uri:match("vault=([^&]+)")
+						local encoded_path = uri:match("filepath=([^&]+)")
+						local uri_toggle = ("obsidian://advanced-uri?vault=%s&filepath=%s&line=%i&commandid=markdown%%3Atoggle-preview"):format(
+							encoded_vault,
+							encoded_path,
+							line - 1
+						)
+						vim.defer_fn(function()
+							vim.ui.open(uri_toggle)
+						end, 100)
+					end
+				end
+			end,
+		},
+		callbacks = {
+			-- pre_write_note = function()
+			-- 	vim.cmd("Obsidian open")
+			-- end,
+			enter_note = function()
+				vim.keymap.del("n", "<CR>", { buffer = true })
+				vim.keymap.del("n", "]o", { buffer = true })
+				vim.keymap.del("n", "[o", { buffer = true })
+				vim.keymap.set("n", "]o", function()
+					require("obsidian.api").nav_link("next")
+					vim.cmd("normal! zz")
+				end, { buffer = true, desc = "Obsidian Next Link" })
+				vim.keymap.set("n", "[o", function()
+					require("obsidian.api").nav_link("prev")
+					vim.cmd("normal! zz")
+				end, { buffer = true, desc = "Obsidian Prev Link" })
+				vim.keymap.set("n", "<leader>c", "<cmd>Obsidian toggle_checkbox<cr>", {
+					buffer = true,
+				})
+			end,
 		},
 		attachments = {
-			folder = "Assets",
+			folder = "_attachments",
 			confirm_img_paste = false,
 		},
 		checkbox = {
@@ -69,6 +109,9 @@ local M = {
 				insert_tag = "<C-l>",
 			},
 		},
+		unique_note = {
+			enabled = false,
+		},
 		note_id_func = function(title)
 			if title ~= nil then
 				return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
@@ -78,8 +121,9 @@ local M = {
 		note = {
 			template = "default.md",
 		},
+		-- TODO: eventually move to luasnip
 		templates = {
-			folder = "Templates",
+			folder = "_templates",
 			substitutions = {
 				dummy = function()
 					return "dummy"
@@ -87,49 +131,25 @@ local M = {
 			},
 			customizations = (function()
 				local result = {}
-				local files = vim.fn.glob(vault .. "/Templates/*.md", false, true)
+				local files = vim.fn.glob(vault .. "/.templates/*.md", false, true)
 				for _, file in ipairs(files) do
 					local name = vim.fn.fnamemodify(file, ":t:r")
 					name = name:sub(1, 1):upper() .. name:sub(2)
 					local key = name:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", "")
-					result[key] = { notes_subdir = "Notes" }
+					result[key] = { notes_subdir = inbox }
 				end
 				return result
 			end)(),
 		},
-		-- TODO:
-		-- https://github.com/obsidian-nvim/obsidian.nvim/wiki/Autocmds
-		-- Do Autocmds to constantly match scroll with of GUI
-		callbacks = {
-			enter_note = function()
-				vim.keymap.del("n", "<CR>", { buffer = true })
-				vim.keymap.del("n", "]o", { buffer = true })
-				vim.keymap.del("n", "[o", { buffer = true })
-				vim.keymap.set("n", "]o", function()
-					require("obsidian.api").nav_link("next")
-					vim.cmd("normal! zz")
-				end, { buffer = true, desc = "Obsidian Next Link" })
-				vim.keymap.set("n", "[o", function()
-					require("obsidian.api").nav_link("prev")
-					vim.cmd("normal! zz")
-				end, { buffer = true, desc = "Obsidian Prev Link" })
-				vim.keymap.set("n", "<leader>c", "<cmd>Obsidian toggle_checkbox<cr>", {
-					buffer = true,
-				})
-			end,
-		},
 		-- matches daily notes core plugin behavior
 		daily_notes = {
 			enabled = true,
-			folder = "Notes",
+			folder = inbox,
 			template = "daily",
 			date_format = "YYYY-MM-DD-dddd",
 			alias_format = nil,
 			default_tags = { "daily" },
 			workdays_only = false,
-		},
-		unique_note = {
-			enabled = false,
 		},
 	},
 }
